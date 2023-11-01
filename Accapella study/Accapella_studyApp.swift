431d5fa4f3fdfb581b7676477168cab2b61c1c0e
Yukichi <naginoyukichi@gmail.com>
2023-11-01 17:09:05 +0900

    swift loginpage

---
+++ Accapella study/Accapella_studyApp.swift
@@ -0,0 +1,27 @@
+//
+//  Accapella_studyApp.swift
+//  Accapella study
+//
+//  Created by 奈木野諭吉 on 2023/11/01.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
+
+@main
+struct Accapella_studyApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pods/Target Support Files/Pods-Accapella study/Pods-Accapella study-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Accapella_study : NSObject
+@end
+@implementation PodsDummy_Pods_Accapella_study
+@end
